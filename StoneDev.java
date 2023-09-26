1a7b89a8421aba4608b969799d29f00033f420a0
Déborafonso <deboragaafonso@gmail.com>
2023-09-25 22:09:15 -0300

    StoneDev em processo

---
+++ StoneDev.java
@@ -0,0 +1,37 @@
+
+import java.util.Random
+
+public class StoneDev extends Creatures {
+    private Random aleatorio = new Random();
+
+    public StoneDev() {
+        getPoder();
+        getAtaque();
+        getDefesa();
+        getVelocidade();
+        getPontosDeVida();
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}

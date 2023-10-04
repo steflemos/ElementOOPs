b0969916f26a6bf39f29011852fe47806f39f577
STÉFANI LEMOS FERREIRA <stefani_l_ferreira@estudante.sesisenai.org.br>
2023-10-04 18:14:40 -0300

    arrumando erros não errados da maquina do senai

--- Creatures/WaveNerd.java
+++ Creatures/WaveNerd.java
@@ -1,11 +1,24 @@
+// package Creatures;
+
+// public class WaveNerd extends Creatures {
+    
+//     @Override
+//     public String toString() {
+//         return String.format("%n *****WaveNerd***** %n Poder: %d %n Ataque: %d %n Defesa: %d %n Velocidade: %d %n Pontos de vida: %d %n", 
+//         getPoder(), getAtaque(), getDefesa(), getVelocidade(), getPontosDeVida());
+//     }
+    
+// }
+
 package Creatures;
 
 public class WaveNerd extends Creatures {
     
     @Override
-    public String toString() {
-        return String.format("%n *****WaveNerd***** %n Poder: %d %n Ataque: %d %n Defesa: %d %n Velocidade: %d %n Pontos de vida: %d %n", 
-        getPoder(), getAtaque(), getDefesa(), getVelocidade(), getPontosDeVida());
+   public String toString() {
+        return String.format(
+                "%n *****WaveNerd***** %n Poder: %d %n Ataque: %d %n Defesa: %d %n Velocidade: %d %n Pontos de vida: %d %n",
+                getPoder(), getAtaque(), getDefesa(), getVelocidade(), getPontosDeVida());
     }
     
 }
